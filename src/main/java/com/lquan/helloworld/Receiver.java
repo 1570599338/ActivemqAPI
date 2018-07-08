763e5f7c6a4857468a3aad54c7905adc9105afff
1570599338@qq.com <1570599338@qq.com>
2018-07-08 17:12:24 +0800

    ActiveMQ的HelloWorld

---
+++ src/main/java/com/lquan/helloworld/Receiver.java
@@ -0,0 +1,42 @@
+package com.lquan.helloworld;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.DeliveryMode;
+import javax.jms.Destination;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+public class Receiver {
+
+	public static void main(String[] args) throws Exception {
+//		 1，需要填写用户名，密码，以及要连接的地址，均使用默认即可，默认端口为：tcp：//localhost：61616
+			ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(
+					ActiveMQConnectionFactory.DEFAULT_USER,
+					ActiveMQConnectionFactory.DEFAULT_PASSWORD, 
+					"tcp://192.168.1.112:61616");
+			// 第二步：connfaction 工厂对象创建一个connection连接，并且调用Connection的Start方法开启连接，conection默认是关闭的
+			 Connection connection =connectionFactory.createConnection();
+			 connection.start();
+			 // 第三步
+			 // 通过connection对象创建session回话（上线文环境对象），用于接受消息，参数配置1为是否使用饿是事务，参数诶值2为简约配置希望小朋
+			 Session session=connection.createSession(Boolean.FALSE, Session.AUTO_ACKNOWLEDGE);
+			 
+			 //第四部：通过Session创建Destination对象，指的是一个客户端用来制定生产消费目标和消费来源的对象，PTP模式Destion被称作为、
+			 Destination destination = session.createQueue("queueLQuan");
+			// 第五部：我们需要通过session创建
+			 MessageConsumer messageConsumer = session.createConsumer(destination);
+			 
+			 while(true){
+				TextMessage msg = (TextMessage)messageConsumer.receive(); 
+				 if(msg==null) break;
+				 System.out.println("收到的内容"+msg.getText());
+				 
+			 }
+			 connection.close();
+		}
+	}

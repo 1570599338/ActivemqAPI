cc3c44741e310bdb02613814c4ae5c58c81cd4a1
1570599338@qq.com <1570599338@qq.com>
2018-08-21 14:15:27 +0800

    URL更改

--- src/main/java/com/lquan/helloworld/Receiver.java
+++ src/main/java/com/lquan/helloworld/Receiver.java
@@ -1,24 +1,20 @@
 package com.lquan.helloworld;
 
-import javax.jms.Connection;
-import javax.jms.ConnectionFactory;
-import javax.jms.DeliveryMode;
-import javax.jms.Destination;
-import javax.jms.MessageConsumer;
-import javax.jms.MessageProducer;
-import javax.jms.Session;
-import javax.jms.TextMessage;
-
 import org.apache.activemq.ActiveMQConnectionFactory;
 
+import javax.jms.*;
+
 public class Receiver {
+
+	private static final String url="tcp://10.206.2.111:61616";
 
 	public static void main(String[] args) throws Exception {
 //		 1，需要填写用户名，密码，以及要连接的地址，均使用默认即可，默认端口为：tcp：//localhost：61616
 			ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(
 					ActiveMQConnectionFactory.DEFAULT_USER,
 					ActiveMQConnectionFactory.DEFAULT_PASSWORD, 
-					"tcp://192.168.1.112:61616");
+					//"tcp://192.168.1.112:61616");
+					"tcp://10.206.2.111:61616");
 			// 第二步：connfaction 工厂对象创建一个connection连接，并且调用Connection的Start方法开启连接，conection默认是关闭的
 			 Connection connection =connectionFactory.createConnection();
 			 connection.start();
---
+++ src/main/java/com/lquan/helloworld/Test.java
@@ -0,0 +1,9 @@
+package com.lquan.helloworld;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("args = [" + args + "]"+"*****************");
+    }
+
+}
